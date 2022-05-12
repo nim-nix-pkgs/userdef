399a149bc82b165ad9c4f0cb217d496e0057eed3
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:36 -0300

    chore: re index userdef

--- flake.nix
+++ flake.nix
@@ -7,6 +7,30 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."userdef-master".dir   = "master";
+  inputs."userdef-master".owner = "nim-nix-pkgs";
+  inputs."userdef-master".ref   = "master";
+  inputs."userdef-master".repo  = "userdef";
+  inputs."userdef-master".type  = "github";
+  inputs."userdef-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."userdef-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."userdef-0_1_0".dir   = "0_1_0";
+  inputs."userdef-0_1_0".owner = "nim-nix-pkgs";
+  inputs."userdef-0_1_0".ref   = "master";
+  inputs."userdef-0_1_0".repo  = "userdef";
+  inputs."userdef-0_1_0".type  = "github";
+  inputs."userdef-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."userdef-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."userdef-0_2_0".dir   = "0_2_0";
+  inputs."userdef-0_2_0".owner = "nim-nix-pkgs";
+  inputs."userdef-0_2_0".ref   = "master";
+  inputs."userdef-0_2_0".repo  = "userdef";
+  inputs."userdef-0_2_0".type  = "github";
+  inputs."userdef-0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."userdef-0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
